478865f6a96bbf58e3bf59ddbc24ca78f2d1e6b8
Kathy Nguyen <kathyvng@gmail.com>
2018-06-21 02:56:06 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,7 @@
 def square_array(array)
-  array = [1,2,3,4]
-  square_array.each
+  square_array =[]
+  square_array.each do |numbers|
+    square_array << numbers * numbers
+  end
+  square_array    
 end
